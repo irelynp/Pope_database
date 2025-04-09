936b78721da47e4ec0d1e540c21c1aedfdd8f256
irelynp <irelynp@students.iwcs.k12.va.us>
2025-04-09 13:12:31 -0400

    initial upload

---
+++ sql.sql
@@ -0,0 +1,7 @@
+--CREATE TABLE USERS (username  TEXT NOT NULL PRIMARY KEY, 
+--password NOT NULL, email NOT NULL);
+
+--NSERT INTO users (username, password, email)
+--VALUES ("Joe", "joespassword", "joes@yahoo.com");
+
+--SELECT * FROM users;
